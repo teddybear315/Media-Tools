8fc4201c903a3692c6c1215401753786816a45bd
Logan Houston <44184086+teddybear315@users.noreply.github.com>
2023-10-27 01:51:31 -0500

    added -ll --broken_artwork, deprecated -tf
    * added -ll for lossless encoding
    * added --broken_artwork
    * removed -tf because it doesnt work
    * fixed movie looping through folder
    * fixed not compatible with srt files that dont have language suffix

--- README.md
+++ README.md
@@ -1,21 +1,22 @@
 # Media Tools
 
  Media tools for reencoding TV/Movie media
- 
+
  For ease of use or if having problems with compare.py running, add this project to your PATH variable and create script files along the lines of `python <install path>/<python script>.py`
- 
+
  for compare please avoid the name "compare"
 
 ## Why would anyone use this?
+
  I frequently find media either only available in x264, not encoded with 10bit, or just having larger file sizes or a higher bitrate than needed. With a large library this tool makes it easy to format entire series or movies.
 
-# Requirements
+## Requirements
 
  FFmpeg
- 
+
  Python
 
-# Usage
+## Usage
 
 ## Recommended Bitrate and Quality for x265 encoding
 
@@ -38,12 +39,13 @@ capital letter in \[y/n\] default
 ### MetaData
 
 
-| argument   | description  | raw data |
-| ---------- | ------------ | -------- |
-| `-m [str]` | Movie Name   | &#9745;  |
-| `-t [str]` | Show Title   | &#9745;  |
-| `-s [int]` | Season       | &#9745;  |
-| `-y [int]` | Release Year | &#9745;  |
+| argument    | description  | raw data |
+| ----------- | ------------ | -------- |
+| `-m [str]`  | Movie Name   | &#9745;  |
+| `-mp [str]` | Movie Path   | &#9745;  |
+| `-t [str]`  | Show Title   | &#9745;  |
+| `-s [int]`  | Season       | &#9745;  |
+| `-y [int]`  | Release Year | &#9745;  |
 
 ### FFmpeg settings
 
@@ -61,11 +63,13 @@ capital letter in \[y/n\] default
 | argument                   | description                                                                                                       | raw data |
 | -------------------------- | ----------------------------------------------------------------------------------------------------------------- | -------- |
 | `-sv`                      | Skip Video Encoding                                                                                               | &#9744;  |
-| `-t[a/f]`                  | Tune Animation/Film                                                                                               | &#9744;  |
+| -ll                        | Lossless conversion, used mainly for heavily compressed x264 conversion                                           | &#9744;  |
+| `-ta`                      | Tune Animation                                                                                                    | &#9744;  |
 | `-lv [encoding lvl]`       | Encoding level, `4.0`:720p60/1080p30, `4.1`:1080p60, `5.0`:4k30, `5.1`:4k60                                       | &#9745;  |
 | `-b [int][k/m] [int][k/m]` | Bitrate Min/Max, leave blank for automatic bitrate                                                                | &#9745;  |
 | `-10b [Y/n]`               | 10 Bit Mode                                                                                                       | &#9744;  |
 | `-cq [int]`                | Constant Bitrate Quality (translates to `-rc vbr -crf [cq]` with cpu or `-rc vbr -cq [cq] -qmax [cq+3]` with gpu) | &#9745;  |
+| `--broken_artwork`         | Only use if artwork stream is broken, or you want to discard all video streams other than 0                       | &#9745;  |
 | `-srt [Y/n]`               | External Subtitles                                                                                                | &#9744;  |
 | `-l <eng>`                 | Subtitle Language Abbreviation for External subtitles or omitting extra subtitle streams                          | &#9745;  |
 | `-ss`                      | Skip Subtitle Encoding                                                                                            | &#9744;  |
--- reencode.py
+++ reencode.py
@@ -35,12 +35,12 @@ def yn_bool(yn: str): return yn.lower() == 'y'
 
 
 video_formats = ["mp4", "mkv"]
-rename = show = overwrite_mode = movie = art_path = lang = ""
+rename = show = overwrite_mode = movie = art_path = lang = movie_path = str_subtitlemapping = ""
 q = -1
 fflvl = "4.1"
 subenc = "copy"
 year = season = cq = subtitle_input = ep_i = start_i = end_i = index_checks = ep = 0
-ruler_2nd = pretty = external_subtitles = replace_periods = skip_subtitles = skip_reencode = has_art = b10_mode = tune_animation = tune_film = gpu = False
+ruler_2nd = pretty = external_subtitles = replace_periods = skip_subtitles = skip_reencode = has_art = b10_mode = tune_animation = gpu = broken_artwork = lossless = False
 ep_check = te_check = bit_check = rp_check = ss_check = sr_check = es_check = rn_check = b10_check = ov_check = False
 bitrate = ("0", "0")
 
@@ -56,6 +56,7 @@ help_message = """[Y/n] defaults to capital letter
 IF USING -P YOU 0 BYTE FILES REMOVE -P AND CHECK FFMPEG OUTPUT AND SET SETTINGS ACCORDINGLY
 MetaData:
 -m  [str]   : Movie Name, passed directly to ffmpeg
+-mp [str]   : Movie Path, passed directly to ffmpeg
 -t  [str]   : Show Title, passed directly to ffmpeg
 -s  [int]   : Season, passed directly to ffmpeg
 -y  [int]   : Release Year, passed directly to ffmpeg
@@ -63,17 +64,19 @@ MetaData:
 FFMPEG:
 -c   [#]	    : Thread count
 -gpu		    : use hevc_nvenc over libx265
--p                  : Pretty Output
--r   [Y/n]          : Rename Files?
--rp  [Y/n]          : Replace Periods
+-p              : Pretty Output
+-r   [Y/n]      : Rename Files?
+-rp  [Y/n]      : Replace Periods
 
 Video:
 -sv                 : Skip Video Encoding
--t[a/f]             : Tune Animation/Film
+-ll                 : Lossless conversion, used mainly for heavily compressed x264 conversion
+-ta                 : Tune Animation
 -lv  [encoding lvl] : Encoding level, 4.0:720p60/1080p30, 4.1:1080p60, 5.0:4k30, 5.1:4k60, passed directly to ffmpeg
 -b   [int]k [int]m  : Bitrate Min/Max, leave blank for automatic bitrate, passed directly to ffmpeg
 -10b [Y/n]          : 10 Bit Mode
 -cq  [int]          : Constant Bitrate Quality (translates to "-rc vbr -crf [cq]" with cpu or "-rc vbr -cq [cq] -qmax [cq+3]" with gpu), passed directly to ffmpeg
+--broken_artwork    : Only use if artwork stream is broken, or you want to discard all video streams other than 0
 
 -srt [Y/n]          : External Subtitles
 -l  <eng>           : Subtitle Language Abbreviation for External subtitles, passed directly to ffmpeg
@@ -97,8 +100,8 @@ for arg in range(0, len(argv)):
         else: print("add -b -q or -cq to get bitrate and quality recommendations based on content\n")
         print(help_message)
         exit(0)
+    elif argv[arg] == "--broken_artwork": broken_artwork = True
     elif argv[arg] == "-ta": tune_animation = True
-    elif argv[arg] == "-tf": tune_film = True
     elif argv[arg] == "-gpu": gpu  = True
     elif argv[arg] == "-p": pretty = True
     elif argv[arg] == "-s": season = int(argv[arg + 1])
@@ -107,6 +110,8 @@ for arg in range(0, len(argv)):
     elif argv[arg] == "-y": year   = int(argv[arg + 1])
     elif argv[arg] == "-ss": ss_check = skip_subtitles = True
     elif argv[arg] == "-sv": sr_check = skip_reencode = True
+    elif argv[arg] == "-ll": lossless = True
+    elif argv[arg] == "-mp": movie_path = argv[arg + 1]
     elif argv[arg] == "-r":  # rename boolean
         rn_check = True
         rename = True if default(arg) else yn_bool(argv[arg + 1])
@@ -231,6 +236,7 @@ def ep_pos_to_int(_str, pos):
 
 # Loop through each item in the directory
 for item in os.listdir(cwd):
+    if movie_path and movie: item = movie_path
     item_path = os.path.join(cwd, item)  # Full path to the item
     # Check if the item is a video file, if not check next file
     if item[-3:] not in video_formats: continue
@@ -278,44 +284,47 @@ for item in os.listdir(cwd):
 
     str_loglevel = ' -loglevel quiet -stats' if pretty else ''
     str_overwrite = ' -' + overwrite_mode if ov_check else ''
-    str_tune = f" -tune {'animation' if tune_animation else ('film' if tune_film else '')}"
+    str_tune = f" -tune animation"
 
     cmd = f"ffmpeg{str_loglevel}{str_overwrite} -hide_banner {'-hwaccel auto' if gpu else ''} -i \"./{item}\""
 
     if has_art: cmd = f"{cmd} -i \"{art_path}\" -map 1 -c:v:1 copy {art_path[-3:]} -disposition:v:1 attatched_pic"
-    if external_subtitles: cmd = f"{cmd} -i \"./{item_title}.{lang}.srt\""
+    if external_subtitles:
+        cmd = f"{cmd} -i \"./{item_title}{'.'+lang if lang else ''}.srt\""
+        str_subtitlemapping = f" -map {'2' if has_art else '1'}"
     if skip_reencode:
-        cmd = f"{cmd} -map 0:v -gpu 0 -c:v:0 copy"
+        cmd = f"{cmd} -map 0:v{str_subtitlemapping} -gpu 0 -c:v:0 copy"
     else:
-        str_subtitlemapping = f' -map ' + ('2' if has_art else '1') if external_subtitles else ''
         str_profile = f"main{'10' if b10_mode else ''}"
         str_maxbitrate = (" -maxrate " + bitrate[1]) if bitrate[1] != 'AUTO' else ''
         if gpu: str_bitmode = "p010le" if b10_mode else "yuv420p"
         else:   str_bitmode = f"yuv420p{'10le' if b10_mode else ''}"
 
-        cmd = f"{cmd} -map 0:v{str_subtitlemapping} -c:v:0 {'hevc_nvenc' if gpu else 'libx265'}" \
+        cmd = f"{cmd} -map 0:v{':0'if broken_artwork else ''}{str_subtitlemapping} -c:v:0 {'hevc_nvenc' if gpu else 'libx265'}" \
               f" -profile:v {str_profile} -level {fflvl} -preset {'p6' if gpu else 'fast'} -pix_fmt {str_bitmode}"
-        if bitrate[0].lower() != "auto":
-            cmd = f"{cmd} -b:v {bitrate[0]}{str_maxbitrate}"
-            if not gpu:
-                if bitrate[1][-1].lower() == 'k': cmd = f"{cmd} -bufsize {int(bitrate[1][:-1])+1000}K"
-                elif bitrate[1][-1].lower() == 'm': cmd = f"{cmd} -bufsize {int(bitrate[1][:-1])+1}M"
-        if q > 0 and gpu: # -gpu -q
-            cmd = f"{cmd} -rc vbr -qp {q} -qmin {q - 3} -qmax {q + 3}"
-        elif q > 0 and not gpu:
-            cmd = f"{cmd} -rc vbr -crf {q}"
-        elif cq and gpu: # -gpu -cq
-            cmd = f"{cmd} -rc vbr -qp {cq} -qmax {cq + 3}"
-        elif cq and not gpu: # -c -cq
-            cmd = f"{cmd} -rc vbr -crf {cq}"
-        elif bitrate[1].lower() == "AUTO" and cq+q == 0: # -b [int]
-            cmd = f"{cmd} -rc cbr{' -qp -1' if gpu else ''}"
-        elif gpu: # -gpu
-            cmd = f"{cmd} -rc vbr -qp 18 -qmax 20"
-        else:
-            cmd = f"{cmd} -rc vbr -crf 20"
-
-    cmd = f"{cmd}{str_tune if tune_film or tune_animation else ''} -aq-mode 2 -map 0:a -c:a copy"
+        if not lossless:
+            if bitrate[0].lower() != "auto":
+                cmd = f"{cmd} -b:v {bitrate[0]}{str_maxbitrate}"
+                if not gpu:
+                    if bitrate[1][-1].lower() == 'k': cmd = f"{cmd} -bufsize {int(bitrate[1][:-1])+1000}K"
+                    elif bitrate[1][-1].lower() == 'm': cmd = f"{cmd} -bufsize {int(bitrate[1][:-1])+1}M"
+            if q > 0 and gpu: # -gpu -q
+                cmd = f"{cmd} -rc vbr -qp {q} -qmin {q - 3} -qmax {q + 3}"
+            elif q > 0 and not gpu:
+                cmd = f"{cmd} -rc vbr -crf {q}"
+            elif cq and gpu: # -gpu -cq
+                cmd = f"{cmd} -rc vbr -qp {cq} -qmax {cq + 3}"
+            elif cq and not gpu: # -c -cq
+                cmd = f"{cmd} -rc vbr -crf {cq}"
+            elif bitrate[1].lower() == "AUTO" and cq+q == 0: # -b [int]
+                cmd = f"{cmd} -rc cbr{' -qp -1' if gpu else ''}"
+            elif gpu: # -gpu
+                cmd = f"{cmd} -rc vbr -qp 18 -qmax 20"
+            else:
+                cmd = f"{cmd} -rc vbr -crf 20"
+            cmd = f"{cmd}{str_tune if tune_animation else ''} -aq-mode 2"
+        else: cmd = f"{cmd} -x265-params lossless=1"
+    cmd = f"{cmd} -map 0:a -c:a copy"
 
     if not skip_subtitles:
         if not lang:
@@ -323,7 +332,7 @@ for item in os.listdir(cwd):
         else:
             cmd = f"{cmd} -map 0:s:m:language:{lang} -c:s {subenc}"
         if external_subtitles:
-            cmd = f"{cmd} -metadata:s:s:0 language={lang}"
+            cmd = f"{cmd} -metadata:s:s:0 language={lang if lang else 'eng'}"
 
     if ep_title: cmd = f"{cmd} -metadata title=\"{ep_title}\""
     elif movie:  cmd = f"{cmd} -metadata title=\"{movie}\""
@@ -343,6 +352,7 @@ for item in os.listdir(cwd):
             ep = nth_item + 1
         else:
             ep = ep + 1
+    if movie: break
 
 if "-cp" in argv:
     subprocess.call(f"python {os.path.dirname(os.path.abspath(__file__))}/compare.py")
