8f222f9dd632c141567371338d547296bf871a4c
Logan Houston <44184086+teddybear315@users.noreply.github.com>
2023-12-24 17:54:30 -0600

    1.0
    compare.py:
    * implemented single mode for single file instead of entire folder
    
    reencode.py:
    * implemented compare script into reencode
    * implemented termcolor for better readability
    * fixed bug where external subtitles would be in 2nd track and audio in 3rd
    * added quality options for lossless encoding
    * minor bug fixes

--- reencode.py
+++ reencode.py
@@ -3,6 +3,8 @@ import os
 import subprocess
 from sys import argv
 
+from termcolor import colored, cprint
+
 # Get the current working directory
 cwd = os.getcwd()
 debug = ["-h", "--help", "-help"] in argv or "-debug" in argv
@@ -35,10 +37,11 @@ def yn_bool(yn: str): return yn.lower() == 'y'
 
 
 video_formats = ["mp4", "mkv"]
-rename = show = overwrite_mode = movie = art_path = lang = movie_path = str_subtitlemapping = ""
+rename = show = overwrite_mode = movie = art_path = lang = movie_path = str_subtitlemapping = output_item = ""
 q = cq = -1
 fflvl = "4.1"
 subenc = "copy"
+percent_history = size_history = []
 year = season = subtitle_input = ep_i = start_i = end_i = index_checks = ep = 0
 ruler_2nd = pretty = external_subtitles = replace_periods = skip_subtitles = skip_reencode = has_art = b10_mode = tune_animation = gpu = broken_artwork = lossless = False
 ep_check = te_check = bit_check = rp_check = ss_check = sr_check = es_check = rn_check = b10_check = ov_check = no_audio_lang = False
@@ -50,7 +53,7 @@ res\tbitrate\tq val
 1080p\t1-3mbps\t20-25\tanimation
 1080p\t2-4mbps\t20-25
 1440p\t4-6mbps\t20-25
-2160p\t6-10mbps\t13-18"""
+2160p\t6-10mbps\t13-18\n"""
 
 help_message = """[Y/n] defaults to capital letter
 IF USING -P YOU 0 BYTE FILES REMOVE -P AND CHECK FFMPEG OUTPUT AND SET SETTINGS ACCORDINGLY
@@ -148,7 +151,7 @@ for arg in range(0, len(argv)):
             bitrate = (argv[arg + 1], "AUTO")
     elif argv[arg] == "-q": q = int(argv[arg + 1])
     elif argv[arg] == "-cq": cq = int(argv[arg + 1])
-    elif argv[arg] == "-lv": fflvl = "5.1" if default(arg) else argv[arg + 1]
+    elif argv[arg] == "-lv": fflvl = "4.1" if default(arg) else argv[arg + 1]
     elif argv[arg] == "-10b":  # 10 bit mode
         b10_check = True
         b10_mode = True if default(arg) else yn_bool(argv[arg + 1])
@@ -247,7 +250,7 @@ for item in os.listdir(cwd):
     if item[-3:] not in video_formats: continue
     item_title = item[:-4]
     ep_title = ""
-    print("File:", item_title)
+    #print("File:", item_title)
     nth_item = 0  # any time nth_item != 0 the current file contains multiple episodes
 
     if rename_decision and not movie:
@@ -279,13 +282,12 @@ for item in os.listdir(cwd):
         # if renaming file, set output_item
         if replace_periods: ep_title = ep_title.replace('.', ' ')
         # SHOW (YEAR) SxxExx(-xx) EPISODE TITLE
-        output_item = f"{show} ({year}) S{season:02d}E{ep:02d}" \
-                      f"{f'-{nth_item:02d}' if (item_title[ep_i + 3] == '-' and ep_i != 0) else ''} {ep_title}"
+        output_item = f"{show} ({year}) S{season:02d}E{ep:02d}{f'-{nth_item:02d}' if (item_title[ep_i + 3] == '-' and ep_i != 0) else ''} {ep_title}"
     elif movie and rename_decision:
         output_item = f"{movie} ({year})"
     else:
         output_item = item_title
-    print(f"Reencoding {item} to {output_item}.mp4 ...")
+    print(colored("Reencoding", "red")+f" {item} to {output_item}.mp4")
 
     str_loglevel = ' -loglevel quiet -stats' if pretty else ''
     str_overwrite = ' -' + overwrite_mode if ov_check else ''
@@ -293,56 +295,56 @@ for item in os.listdir(cwd):
 
     cmd = f"ffmpeg{str_loglevel}{str_overwrite} -hide_banner {'-hwaccel auto' if gpu else ''} -i \"./{item}\""
 
-    if has_art: cmd = f"{cmd} -i \"{art_path}\" -map 1 -c:v:1 copy {art_path[-3:]} -disposition:v:1 attatched_pic"
     if external_subtitles:
         cmd = f"{cmd} -i \"./{item_title}{'.'+lang if lang else ''}.srt\""
-        str_subtitlemapping = f" -map {'2' if has_art else '1'}"
+        str_subtitlemapping = f" -map 1"
+    if has_art: cmd = f"{cmd} -i \"{art_path}\" -map {'2' if external_subtitles else '1'} -c:v:1 copy {art_path[-3:]} -disposition:v:1 attatched_pic"
     if skip_reencode:
-        cmd = f"{cmd} -map 0:v{str_subtitlemapping} -gpu 0 -c:v:0 copy"
+        cmd = f"{cmd} -map 0:v{':0' if broken_artwork else ''} -gpu 0 -c:v{':0' if broken_artwork else ''} copy"
     else:
         str_profile = f"main{'10' if b10_mode else ''}"
         str_maxbitrate = (" -maxrate " + bitrate[1]) if bitrate[1].lower() != 'auto' else f' -maxrate {bitrate[0]}'
         if gpu: str_bitmode = "p010le" if b10_mode else "yuv420p"
         else:   str_bitmode = f"yuv420p{'10le' if b10_mode else ''}"
 
-        cmd = f"{cmd} -map 0:v{':0' if broken_artwork else ''}{str_subtitlemapping} -c:v{':0' if broken_artwork else ''} {'hevc_nvenc' if gpu else 'libx265'}" \
+        cmd = f"{cmd} -map 0:v{':0' if broken_artwork else ''} -c:v{':0' if broken_artwork else ''} {'hevc_nvenc' if gpu else 'libx265'}" \
               f" -profile:v {str_profile} -level {fflvl} -preset {'p6' if gpu else 'faster'} -pix_fmt {str_bitmode}"
-        if not lossless:
-            if bitrate[0].lower() != "auto":
-                cmd = f"{cmd} -b:v {bitrate[0]}{str_maxbitrate}"
-                if bitrate[1][-1].lower() == 'k': cmd = f"{cmd} -bufsize {int(bitrate[1][:-1])+1000}K"
-                elif bitrate[1][-1].lower() == 'm': cmd = f"{cmd} -bufsize {int(bitrate[1][:-1])+1}M"
-            if q > 0 and gpu: # -gpu -q
-                cmd = f"{cmd} -rc vbr -qp {q} -qmin {q - 3} -qmax {q + 3}"
-            elif q > 0 and not gpu:
-                cmd = f"{cmd} -rc vbr -crf {q}"
-            elif cq > 0 and gpu: # -gpu -cq
-                cmd = f"{cmd} -rc vbr -qp {cq} -qmax {cq + 3}"
-            elif cq > 0 and not gpu: # -c -cq
-                cmd = f"{cmd} -rc vbr -crf {cq}"
-            elif bitrate[1].lower() == "auto" and cq+q <= 0: # -b [int]
-                cmd = f"{cmd} -rc cbr{' -qp -1' if gpu else ''}"
-            # elif gpu: # -gpu
-            #     cmd = f"{cmd} -rc vbr -qp 18 -qmax 20"
-            # else:
-            #     cmd = f"{cmd} -rc vbr -crf 20"
-            cmd = f"{cmd}{str_tune if tune_animation else ''} -aq-mode 2"
-        else: cmd = f"{cmd} -x265-params lossless=1"
-        
-        if lang and not no_audio_lang:
-            cmd = f"{cmd} -map 0:a:m:language:{lang} -c:a copy"
-        elif no_audio_lang:
-            cmd = f"{cmd} -map 0:a -metadata:s:a language={lang} -c:a copy"
-        else:
-            cmd = f"{cmd} -map 0:a -c:a copy"
+        if lossless: cmd = f"{cmd} -x265-params lossless=1"
+        # if not lossless:
+        if bitrate[0].lower() != "auto":
+            cmd = f"{cmd} -b:v {bitrate[0]}{str_maxbitrate}"
+            if bitrate[1][-1].lower() == 'k': cmd = f"{cmd} -bufsize {int(bitrate[1][:-1])+1000}K"
+            elif bitrate[1][-1].lower() == 'm': cmd = f"{cmd} -bufsize {int(bitrate[1][:-1])+1}M"
+        if q > 0 and gpu: # -gpu -q
+            cmd = f"{cmd} -rc vbr -qp {q} -qmin {q - 3} -qmax {q + 3}"
+        elif q > 0 and not gpu:
+            cmd = f"{cmd} -rc vbr -crf {q}"
+        elif cq > 0 and gpu: # -gpu -cq
+            cmd = f"{cmd} -rc vbr -qp {cq} -qmax {cq + 3}"
+        elif cq > 0 and not gpu: # -c -cq
+            cmd = f"{cmd} -rc vbr -crf {cq}"
+        elif bitrate[1].lower() == "auto" and cq+q <= 0: # -b [int]
+            cmd = f"{cmd} -rc cbr{' -qp -1' if gpu else ''}"
+        # elif gpu: # -gpu
+        #     cmd = f"{cmd} -rc vbr -qp 18 -qmax 20"
+        # else:
+        #     cmd = f"{cmd} -rc vbr -crf 20"
+        cmd = f"{cmd}{str_tune if tune_animation else ''} -aq-mode 2"
+        # else: cmd = f"{cmd} -x265-params lossless=1"
+    if lang and not no_audio_lang:
+        cmd = f"{cmd} -map 0:a:m:language:{lang} -c:a copy"
+    elif no_audio_lang:
+        cmd = f"{cmd} -map 0:a -metadata:s:a language={lang} -c:a copy"
+    else:
+        cmd = f"{cmd} -map 0:a -c:a copy"
 
     if not skip_subtitles:
         if not lang:
-            cmd = f"{cmd}{' -map 0:s' if not str_subtitlemapping else ''} -c:s {subenc}"
+            cmd = f"{cmd}{' -map 0:s' if not str_subtitlemapping else str_subtitlemapping} -c:s {subenc}"
         elif not external_subtitles:
             cmd = f"{cmd} -map 0:s:m:language:{lang} -c:s {subenc}"
         else:
-            cmd = f"{cmd} -metadata:s:s:0 language={lang if lang else 'eng'} -c:s {subenc}"
+            cmd = f"{cmd}{str_subtitlemapping} -metadata:s:s:0 language={lang if lang else 'eng'} -c:s {subenc}"
 
     if ep_title: cmd = f"{cmd} -metadata title=\"{ep_title}\""
     elif movie:  cmd = f"{cmd} -metadata title=\"{movie}\""
@@ -352,7 +354,7 @@ for item in os.listdir(cwd):
     if year:     cmd = f"{cmd} -metadata year={year}"
 
     cmd = f"{cmd} -f mp4 \"{os.getcwd()}/Reencode/{output_item}.mp4\""
-    print(cmd)
+    print(colored("Command ", "yellow") + cmd)
     if not debug:
         subprocess.call(cmd)
 
@@ -364,5 +366,56 @@ for item in os.listdir(cwd):
             ep = ep + 1
     if movie: break
 
-if "-cp" in argv:
-    subprocess.call(f"python {os.path.dirname(os.path.abspath(__file__))}/compare.py")
+    if "-cp" in argv:
+        if movie:
+            file1_path = movie_path
+            file2_path = f"{cwd}/Reencode/{output_item}.mp4"
+        else:
+            file1_path = f"{cwd}/{item}"
+            file2_path = f"{cwd}/Reencode/{output_item}.mp4"
+
+        total_start_size = total_end_size = 0
+        size1 = round(os.path.getsize(file1_path)/1024/1024)
+        size2 = round(os.path.getsize(file2_path)/1024/1024)
+        size_history.append((size1,size2))
+        size_char1 = size_char2 ='M'
+        total_start_size += size1
+        total_end_size += size2
+
+        size_difference = size1 - size2
+        percentage_difference = (size_difference / size1) * 100
+        percent_history.append(percentage_difference)
+
+        if size1 > 974: # 95% of GiB
+            size1 = size1 / 1024
+            size_char1 = "G"
+        if size2 > 974:
+            size2 = size2 / 1024
+            size_char2 = "G"
+        print(colored("File Size", "green")+f" Original: {size1:.2f} {size_char1}iB, Reencoded: {size2:.2f} {size_char2}iB, {colored('reduction', 'green')}: {percentage_difference:.2f}%")
+
+if len(percent_history) > 1 and "-cp" in argv:
+    cprint("Overall Reduction History", "green")
+    total_p = 0
+    total_start_size = total_end_size = 0
+    size_char1 = size_char2 ='M'
+    for i in range(0,len(percent_history)):
+        total_p += percent_history[i]
+        total_start_size += size_history[i][0]
+        total_end_size += size_history[i][1]
+        if size_history[i][0] > 974: # 95% of GiB
+            size_history[i][0] = size_history[i][0] / 1024
+            size_char1 = "G"
+        if size_history[i][1] > 974:
+            size_history[i][1] = size_history[i][1] / 1024
+            size_char2 = "G"
+        print(colored("Total File Size", "green")+f" Original: {size_history[i][0]:.2f} {size_char1}iB, Reencoded: {size_history[i][1]:.2f} {size_char2}iB, {colored('reduction', 'green')}: {percent_history[i]:.2f}%")
+    avg_p = total_p / len(percent_history)
+    size_char1 = size_char2 ='M'
+    if total_start_size > 974: # 95% of GiB
+        total_start_size = total_start_size / 1024
+        size_char1 = "G"
+    if total_end_size > 974:
+        total_end_size = total_end_size / 1024
+        size_char2 = "G"
+    print(colored("Total File Size", "green")+f" Original: {total_start_size:.2f} {size_char1}iB, Reencoded: {total_end_size:.2f} {size_char2}iB, {colored('reduction', 'green')}: {avg_p:.2f}%")
